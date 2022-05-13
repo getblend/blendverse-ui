22d8e53b387ed2ebbda91ab74b9762c6cc73b811
THEJASVEEM <thejavseem7@gmail.com>
2022-05-13 15:48:47 +0530

    Implemented fallback behaviour
    Implemented fallback behaviour with foreground image fails then, text with user's initials triggers, and if text fails then background image triggers

--- lib/src/widgets/avatar.dart
+++ lib/src/widgets/avatar.dart
@@ -1,8 +1,12 @@
+import 'dart:math';
+
 import 'package:blendverse_ui/src/utils/widget_size.dart';
 import 'package:flutter/material.dart';
 
 /// Component used to display user avatar picture.
 ///
+/// If [src] fails then [fallbackName] is used. If
+/// [fallbackName] fails too, [_backgroundImage] is used.
 /// USAGE:
 /// {@tool snippet}
 ///
@@ -28,11 +32,24 @@ import 'package:flutter/material.dart';
 /// )
 /// ```
 /// {@end-tool}
+///
+/// {@tool snippet}
+///
+/// If the avatar needs to have a fallback name,
+/// the username needs to be passed on to [fallbackName]
+///
+///
+/// ```dart
+/// Avatar(
+///   fallbackName: 'Lorem Ipsum'
+/// )
+/// ```
 class Avatar extends StatelessWidget {
   const Avatar({
     Key? key,
     required this.src,
     this.size = WidgetSize.md,
+    this.fallbackName = '',
   }) : super(key: key);
 
   /// The URL of the image from where to fetch the data.
@@ -45,6 +62,11 @@ class Avatar extends StatelessWidget {
   ///The argument [size] takes md = 32 as the default property
   ///
   final WidgetSize size;
+
+  /// The full name of the avatar/user
+  ///
+  /// The arguments [fallbackName] must not be null
+  final String fallbackName;
 
   /// dart getter function [_radius] to alter the size of the avatar
   double get _radius {
@@ -62,11 +84,50 @@ class Avatar extends StatelessWidget {
     }
   }
 
+  /// dart getter function [_clipName] to get the initials from username
+  /// as a primary fallback
+  String get _clipName {
+    final trimName = fallbackName.trim();
+
+    if (fallbackName.isNotEmpty && trimName.split(' ').length > 1) {
+      return trimName.split(' ').map((l) => l[0]).take(2).join().toUpperCase();
+    } else if (fallbackName.trim().split(' ').length == 1) {
+      return trimName.characters.map((l) => l[0]).take(2).join().toUpperCase();
+    } else {
+      return '';
+    }
+  }
+
+  ///dart getter function [_backgroundColor] to get set random background
+  ///colors from Colors.primaries
+  Color? get _backgroundColor {
+    return Colors.primaries[Random().nextInt(Colors.primaries.length)];
+  }
+
+  ///dart getter function [_backgroundImage] to get image only if username field
+  ///is null as a secondary fallback
+  ImageProvider? get _backgroundImage {
+    if (_clipName.isEmpty) {
+      return const NetworkImage(
+        'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_960_720.png',
+      );
+    }
+    return null;
+  }
+
   @override
   Widget build(BuildContext context) {
     return CircleAvatar(
-      backgroundImage: NetworkImage(src),
       radius: _radius,
+      foregroundImage: NetworkImage(src),
+      backgroundImage: _backgroundImage,
+      backgroundColor: _backgroundColor,
+      child: _clipName.isEmpty
+          ? null
+          : Text(
+              _clipName,
+              style: TextStyle(fontSize: _radius),
+            ),
     );
   }
 }
