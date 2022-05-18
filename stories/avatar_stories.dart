0fb23953b567dc2d976f627d746380065a485722
Thejasvee Murthy <thejasveemurthy@Viveks-MacBook-Pro.local>
2022-05-18 16:24:05 +0530

    Fixed review changes
    Implemented tap behaviour on monarch stories

--- stories/avatar_stories.dart
+++ stories/avatar_stories.dart
@@ -1,32 +1,52 @@
+// ignore_for_file: avoid_print
+
 import 'package:blendverse_ui/widgets.dart';
 import 'package:flutter/material.dart';
 
 Widget avatars() => Column(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: const [
+      children: [
         Avatar(
           src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
+          onTap: () {
+            print('Tapped on avatar');
+          },
         ),
         Avatar(
           src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
           size: WidgetSize.xs,
+          onTap: () {
+            print('Tapped on avatar');
+          },
         ),
         Avatar(
           src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
           size: WidgetSize.sm,
+          onTap: () {
+            print('Tapped on avatar');
+          },
         ),
         Avatar(
           src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
           // ignore: avoid_redundant_argument_values
           size: WidgetSize.md,
+          onTap: () {
+            print('Tapped on avatar');
+          },
         ),
         Avatar(
           src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
           size: WidgetSize.lg,
+          onTap: () {
+            print('test');
+          },
         ),
         Avatar(
           src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
           size: WidgetSize.xl,
+          onTap: () {
+            print('test');
+          },
         )
       ],
     );
