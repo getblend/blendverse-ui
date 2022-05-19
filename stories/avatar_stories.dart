c4a11b3c44af39616c0f9d559b482212c1e4f7b3
Thejasvee Murthy <thejasveemurthy@Viveks-MacBook-Pro.local>
2022-05-19 12:53:54 +0530

    Implemented separate component in the stories

--- stories/avatar_stories.dart
+++ stories/avatar_stories.dart
@@ -3,50 +3,61 @@
 import 'package:blendverse_ui/widgets.dart';
 import 'package:flutter/material.dart';
 
-Widget avatars() => Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+Widget avatars() => Row(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Avatar(
-          src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
-          onTap: () {
-            print('Tapped on avatar');
-          },
+        Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(
+                'Default Avatar',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+            const Avatar(
+              src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
+            ),
+            const Text(
+              'Avatar Sizes',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            const Avatar(
+              src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
+              size: WidgetSize.xs,
+            ),
+            const Avatar(
+              src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
+              size: WidgetSize.sm,
+            ),
+            const Avatar(
+              src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
+              // ignore: avoid_redundant_argument_values
+              size: WidgetSize.md,
+            ),
+            const Avatar(
+              src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
+              size: WidgetSize.lg,
+            ),
+            const Avatar(
+              src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
+              size: WidgetSize.xl,
+            ),
+            const Text(
+              'Avatar onTap',
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            Avatar(
+              src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
+              onTap: () {
+                print('On Tap/Pressed Avatar');
+              },
+            )
+          ],
         ),
-        Avatar(
-          src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
-          size: WidgetSize.xs,
-          onTap: () {
-            print('Tapped on avatar');
-          },
-        ),
-        Avatar(
-          src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
-          size: WidgetSize.sm,
-          onTap: () {
-            print('Tapped on avatar');
-          },
-        ),
-        Avatar(
-          src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
-          // ignore: avoid_redundant_argument_values
-          size: WidgetSize.md,
-          onTap: () {
-            print('Tapped on avatar');
-          },
-        ),
-        Avatar(
-          src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
-          size: WidgetSize.lg,
-          onTap: () {
-            print('test');
-          },
-        ),
-        Avatar(
-          src: 'https://miro.medium.com/max/250/0*0fClPmIScV5pTLoE.jpg',
-          size: WidgetSize.xl,
-          onTap: () {
-            print('test');
-          },
-        )
       ],
     );
