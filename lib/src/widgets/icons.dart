c6c058099082e6f8d199e48863a7a6ed4962a650
Thejasvee Murthy <thejasveemurthy@Viveks-MacBook-Pro.local>
2022-05-25 18:17:29 +0530

    Implemented Icon Sizes
    Implemented size getter function for the size of the icon, getting size from WidgetSize, implemented a story for the same

--- lib/src/widgets/icons.dart
+++ lib/src/widgets/icons.dart
@@ -1,4 +1,5 @@
 import 'package:blendverse_ui/src/utils/blend_icons.dart';
+import 'package:blendverse_ui/src/utils/widget_size.dart';
 import 'package:flutter/material.dart';
 
 /// Component used to display icons
@@ -11,7 +12,8 @@ import 'package:flutter/material.dart';
 ///
 /// ```dart
 /// BlendIcon(
-///   blendIcons: BlendIcons.reaction
+///   blendIcons: BlendIcons.reaction,
+///   sise: WidgetSize.lg,
 /// )
 /// ```
 /// {@end-tool}
@@ -20,6 +22,7 @@ class BlendIcon extends StatelessWidget {
   const BlendIcon({
     Key? key,
     required this.blendIcons,
+    this.size = WidgetSize.md,
   }) : super(key: key);
 
   /// The [blendIcons] takes in [BlendIcons] enum, which icon to load
@@ -27,6 +30,12 @@ class BlendIcon extends StatelessWidget {
   /// The arguments [blendIcons] must not be null.
   final BlendIcons blendIcons;
 
+  /// Controls the size of the icon
+  ///
+  /// The argument [size] takes md = 32 as the default property
+  final WidgetSize size;
+
+  /// Dart getter function to get [IconData] from the enum [BlendIcons]
   IconData? get _getIcon {
     switch (blendIcons) {
       case BlendIcons.rection:
@@ -41,8 +50,27 @@ class BlendIcon extends StatelessWidget {
     return null;
   }
 
+  /// Dart getter function [_size] to alter the size of the icon
+  int get _size {
+    switch (size) {
+      case WidgetSize.xs:
+        return 24;
+      case WidgetSize.sm:
+        return 36;
+      case WidgetSize.md:
+        return 48;
+      case WidgetSize.lg:
+        return 72;
+      case WidgetSize.xl:
+        return 108;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Icon(_getIcon);
+    return Icon(
+      _getIcon,
+      size: _size.toDouble(),
+    );
   }
 }
