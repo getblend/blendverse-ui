a9889df580d9dabe9627ade6253a607267f51331
THEJASVEE MURTHY <43312159+THEJASVEEM@users.noreply.github.com>
2022-05-28 01:58:43 +0530

    Implemented fallback behaviour (#6)
    * Implemented fallback behaviour
    
    Implemented fallback behaviour with foreground image fails then, text with user's initials triggers, and if text fails then background image triggers
    
    * Fixes after review
    
    Fixed space constant with regex, dynamic backgorund color logic, static resource for fall background image added
    
    * Fixed random color with extension, implemented separate story for fallback behaviour
    
    * fix: merging caching and other changes
    
    * Fixes to fallback using errorWidget
    
    * Modified avatar fallback
    
    * fix: pr feedback and better fallbacks
    
    * fix: removing unused asset
    
    Co-authored-by: THEJASVEEM <thejavseem7@gmail.com>
    Co-authored-by: Thejasvee Murthy <thejasveemurthy@Viveks-MacBook-Pro.local>
    Co-authored-by: WrathOfZombies <bhargav.krishna@outlook.com>
    Co-authored-by: Bhargav Krishna <3244360+WrathOfZombies@users.noreply.github.com>

---
+++ lib/src/utils/random_color.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+/// dart extension on color to get set random background
+/// colors from Colors.primaries
+
+extension RandomColor on Color {
+  static Color random(int? seed) {
+    final colors = <Color>[
+      Colors.red,
+      Colors.blue,
+      Colors.green,
+      Colors.yellow,
+      Colors.orange,
+      Colors.purple,
+      Colors.indigo,
+    ];
+    if (seed == null || seed == 0) {
+      return colors[Random().nextInt(colors.length)];
+    }
+    return colors[seed % colors.length];
+  }
+}
