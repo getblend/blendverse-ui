cf686167d4ab901526ea28684b1c8c688315bbf3
Thejasvee Murthy <thejasveemurthy@Viveks-MacBook-Pro.local>
2022-06-03 11:39:36 +0530

    Implement types prop
    Implemented types enum and property, implemented a story to demo

--- example/lib/src/stories/typography_stories.dart
+++ example/lib/src/stories/typography_stories.dart
@@ -14,3 +14,65 @@ Widget basic() => const Story(
         ),
       ],
     );
+
+Widget styles() => const Story(
+      'Available Styles',
+      [
+        Typography(
+          'Heading 1',
+          style: TypoStyle.h1,
+        ),
+        Typography(
+          'Heading 2',
+          style: TypoStyle.h2,
+        ),
+        Typography(
+          'Heading 3',
+          style: TypoStyle.h3,
+        ),
+        Typography(
+          'Heading 4',
+          style: TypoStyle.h4,
+        ),
+        Typography(
+          'Heading 5',
+          style: TypoStyle.h5,
+        ),
+        Typography(
+          'Heading 6',
+          style: TypoStyle.h6,
+        ),
+        Typography(
+          'Body 1',
+          style: TypoStyle.b1,
+        ),
+        Typography(
+          'Body2',
+          style: TypoStyle.b2,
+        ),
+        Typography(
+          'Subtitle 1',
+          style: TypoStyle.s1,
+        ),
+        Typography(
+          'Subtitle 2',
+          style: TypoStyle.s2,
+        ),
+        Typography(
+          'Caption',
+          style: TypoStyle.caption,
+        ),
+        Typography(
+          'Error',
+          style: TypoStyle.err,
+        ),
+        Typography(
+          'Primary',
+          style: TypoStyle.primary,
+        ),
+        Typography(
+          'Secondary',
+          style: TypoStyle.secondary,
+        ),
+      ],
+    );
--- lib/src/widgets/typography.dart
+++ lib/src/widgets/typography.dart
@@ -1,8 +1,9 @@
+import 'package:blendverse_ui/src/utils/custom_styles.dart';
 import 'package:flutter/material.dart';
 
 /// Component used to display text.
 ///
-/// Takes string [text] as required property
+/// Takes string text as required property
 /// USAGE:
 /// {@tool snippet}
 ///
@@ -11,15 +12,36 @@ import 'package:flutter/material.dart';
 ///
 /// ```dart
 /// Typogrpahy(
-///   'Lorem Ipsum'
+///   'Lorem Ipsum',
+/// style: TypoStyle.h3,
 /// )
 /// ```
 /// {@end-tool}
 ///
+
+/// An enum that has a list of text styles available
+enum TypoStyle {
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  b1,
+  b2,
+  s1,
+  s2,
+  caption,
+  err,
+  primary,
+  secondary,
+}
+
 class Typography extends StatelessWidget {
   const Typography(
     this.text, {
     Key? key,
+    this.style = TypoStyle.h4,
   }) : super(key: key);
 
   /// [text] is a required property for the text to displayed
@@ -27,10 +49,52 @@ class Typography extends StatelessWidget {
   /// it takes a string variable
   final String text;
 
+  /// [style] is a property that takes [TypoStyle] enum as the values
+  ///
+  /// By default it takes TypoStyle.h4 as the style
+  final TypoStyle style;
+
+  /// [_style] is a private function for [Typography] class to dynamically
+  ///
+  /// assign the text theme context
+  TextStyle? _style(BuildContext c) {
+    switch (style) {
+      case TypoStyle.h1:
+        return Theme.of(c).textTheme.headline1;
+      case TypoStyle.h2:
+        return Theme.of(c).textTheme.headline2;
+      case TypoStyle.h3:
+        return Theme.of(c).textTheme.headline3;
+      case TypoStyle.h4:
+        return Theme.of(c).textTheme.headline4;
+      case TypoStyle.h5:
+        return Theme.of(c).textTheme.headline5;
+      case TypoStyle.h6:
+        return Theme.of(c).textTheme.headline6;
+      case TypoStyle.b1:
+        return Theme.of(c).textTheme.bodyText1;
+      case TypoStyle.b2:
+        return Theme.of(c).textTheme.bodyText2;
+      case TypoStyle.s1:
+        return Theme.of(c).textTheme.subtitle1;
+      case TypoStyle.s2:
+        return Theme.of(c).textTheme.subtitle2;
+      case TypoStyle.caption:
+        return Theme.of(c).textTheme.caption;
+      case TypoStyle.err:
+        return Theme.of(c).textTheme.error;
+      case TypoStyle.primary:
+        return Theme.of(c).textTheme.primary;
+      case TypoStyle.secondary:
+        return Theme.of(c).textTheme.secondary;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Text(
       text,
+      style: _style(context),
     );
   }
 }
