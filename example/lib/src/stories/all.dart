63e723a905a449533c575b4dc67dd793b1b13fd4
THEJASVEE MURTHY <43312159+THEJASVEEM@users.noreply.github.com>
2022-06-06 13:52:22 +0530

    Implement Typography Widget (#18)
    Implemented typography with stories
    
    Co-authored-by: Thejasvee Murthy <thejasveemurthy@Viveks-MacBook-Pro.local>

--- example/lib/src/stories/all.dart
+++ example/lib/src/stories/all.dart
@@ -1,11 +1,19 @@
 import 'avatar_stories.dart' as avatar;
 import 'icon_button_stories.dart' as icon_buttons;
+import 'typography_stories.dart' as typography;
 
 var allStories = {
-  'Avatar': [avatar.basic(), avatar.sizes(), avatar.fallbacks()],
+  'Avatar': [
+    avatar.basic(),
+    avatar.sizes(),
+    avatar.fallbacks(),
+  ],
   'Icon Button': [
     icon_buttons.basic(),
     icon_buttons.sizes(),
-    icon_buttons.counter(),
+    icon_buttons.counter()
+  ],
+  'Typography': [
+    typography.basic(),
   ]
 };
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,3 +1,4 @@
 export './src/utils/utils.dart';
 export './src/widgets/avatar.dart';
 export './src/widgets/icon_button.dart';
+export './src/widgets/typography.dart';
