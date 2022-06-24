bbb4a49795844a500701ac296c5e206dc5099d62
vivi173 <43881248+vivi173@users.noreply.github.com>
2022-06-24 16:42:15 +0530

    Update widgets.dart (#27)

--- lib/widgets.dart
+++ lib/widgets.dart
@@ -1,4 +1,4 @@
 export 'src/utils/utils.dart';
-export 'src/widgets/Typography/typography.dart';
+export 'src/widgets/typography/typography.dart';
 export 'src/widgets/avatar.dart';
 export 'src/widgets/icon_button.dart';
